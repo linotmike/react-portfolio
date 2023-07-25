4dc4945c86c9328ab9347c637ae4bfe82ceba141
linotmike <128604219+linotmike@users.noreply.github.com>
2023-07-25 13:17:50 -0700

    updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,21 +3,53 @@ import React from "react";
 export default function About() {
   return (
     <section>
-        <br/>
-        <br/>
-      <div style={{borderRadius:'25px'}} className="card">
+      <br />
+      <br />
+      <div style={{ borderRadius: "25px" }} className="card">
         <div className="card-text">
           <div className="portada"></div>
           <div className="title-total">
-            <div className="title">Upcoming software developer</div>
+            <div className="title">Software engineer</div>
             <h2>Birhanu Mengistu</h2>
 
             <div className="desc">
               {" "}
-              I was born and raised in Ethiopia, addis ababa which is the
-              capital city. I am 22 years old and i have an associates degree in
-              aviation. I have a private pilot license with 217 hrs of flight time in a cessna 172/152. I am currently taking a full-stack-web
-              development couse at university of washington.{" "}
+              <ul>
+                <li> My name is Birhanu Mengistu i am a software engineer. </li>
+                <li>
+                  {" "}
+                  I Specialize in Application development, software
+                  architecture, and product innovation Skilled in full-stack
+                  software development.
+                </li>
+                <li>
+                  I design layouts for applications, creating and maintaining
+                  database designs, and troubleshooting.
+                </li>
+                <li>
+                  {" "}
+                  Information technology professional specializing in the
+                  development and launch of innovative Web-based solutions that
+                  best suit user needs.
+                </li>
+                <li>
+                  {" "}
+                  Well-versed in all facets of the software development
+                  lifecycle (SDLC), including requirements gathering, coding and
+                  deployment.
+                </li>
+                <li>
+                  {" "}
+                  Expertise in identifying and resolving technical issues during
+                  software development, as well as providing post launch
+                  technical support.
+                </li>
+                <li>
+                  skilled in collaborating with cross-functional teams to
+                  establish project objectives and create deliverables that can
+                  be produced within strict budget and time parameters.
+                </li>{" "}
+              </ul>
             </div>
           </div>
         </div>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,31 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Document, Page, } from "react-pdf";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudfare.com`
 
-export default function Resume (){
-     return (
-        <section className='resume'>
-            <h1>Resume</h1>
-            <hr/>
-            <h2 className='col-12 d-flex'>Front end proficeinces:</h2>
-            <ul>
-                <li>Html</li>
-                <li>Css</li>
-                <li>Javascript</li>
-                <li>Jquery</li>
-                <li>Responsive design</li>
-                <li>React</li>
-                <li>Bootstrap</li>
-            </ul>
-            <h2>Back end proficeinces:</h2>
-            <ul>
-                <li>Api's</li>
-                <li>Node</li>
-                <li>Express</li>
-                <li>Mysql, sequelize</li>
-                <li>MongoDB, mongoose</li>
-                <li>Rest</li>
-                
-            </ul>
-            <h2>Resume coming soon...</h2>
-        </section>
-     )
+export default function Resume() {
+  return (
+    <div className="pdf">
+      <h2>Resume</h2>
+      <Document
+        file={
+          "https://online.flippingbook.com/view/422773771/"
+        }
+        onLoadError={console.error} 
+        style={{width: '100vh' , height: 'auto'}} >
+            
+        <Page pageIndex={0} />
+      </Document>
+    </div>
+  );
 }
+//     <section className='resume'>
+//         <h1>Resume</h1>
+//         <hr/>
+//         <h2 className='col-12 d-flex'>Front end proficeinces:</h2>
+//         <ul>
+//             <li>Html</li>
+//             <li>Css</li>
+//             <li>Javascript</li>
+//             <li>Jquery</li>
+//             <li>Responsive design</li>
+//             <li>React</li>
+//             <li>Bootstrap</li>
+//         </ul>
+//         <h2>Back end proficeinces:</h2>
+//         <ul>
+//             <li>Api's</li>
+//             <li>Node</li>
+//             <li>Express</li>
+//             <li>Mysql, sequelize</li>
+//             <li>MongoDB, mongoose</li>
+//             <li>Rest</li>
+
+//         </ul>
+//         <h2>Resume coming soon...</h2>
+//     </section>
+//  )
